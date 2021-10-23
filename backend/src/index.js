be78a07d27bf3e7cb463129f0dd8ba932b99d6b9
danluigitating <danluigitating@hotmail.com>
2021-10-23 19:06:00 +0800

    Add cors package for backend [TATINDA]

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 9090
 
 app.use(bodyParser.json())
+app.use(cors())
 app.use(
     bodyParser.urlencoded({
         extended: true,

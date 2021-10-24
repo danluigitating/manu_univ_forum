7265ec338edbe605e2583dc1b69c1c32770c9794
danluigitating <danluigitating@hotmail.com>
2021-10-24 19:26:58 +0800

    Add routing for UI [TATINDA]

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,10 @@
-import {Menu, Layout} from 'antd';
+import {Menu, Layout} from 'antd'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom"
 import {
     HomeOutlined,
     FormOutlined,
@@ -6,55 +12,109 @@ import {
     LikeOutlined,
     DislikeOutlined,
     SettingOutlined
-} from '@ant-design/icons';
-import React from "react";
-import Posts from "./Posts";
+} from '@ant-design/icons'
+import React, {useState} from "react"
+import Posts from "./Posts"
 
-const {Header, Content, Footer, Sider} = Layout;
+const {Header, Content, Footer, Sider} = Layout
 
 export default function Sidebar() {
 
+    const [headerText, setHeaderText] = useState('News Feed')
+
     return (
-        <Layout style={{minHeight: '100vh'}}>
-            <Sider>
-                <h1 style={{
-                    marginLeft: 25,
-                    marginTop: 20,
-                    marginBottom: 15,
-                    color: 'rgba(255, 255, 255, 0.65)'
-                }}>Anonymous Blog</h1>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1" icon={<HomeOutlined/>}>
-                        Home
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<FormOutlined/>}>
-                        My Posts
-                    </Menu.Item>
-                    <Menu.Item key="3" icon={<CommentOutlined/>}>
-                        My Comments
-                    </Menu.Item>
-                    <Menu.Item key="4" icon={<LikeOutlined/>}>
-                        My Upvotes
-                    </Menu.Item>
-                    <Menu.Item key="5" icon={<DislikeOutlined/>}>
-                        My Downvotes
-                    </Menu.Item>
-                    <Menu.Item key="6" icon={<SettingOutlined/>}>
-                        Preferences
-                    </Menu.Item>
-                </Menu>
-            </Sider>
-            <Layout>
-                <Header className="site-layout-sub-header-background"
-                        style={{height: 64, textAlign: 'center', color: "white", fontSize: 20}}>News Feed</Header>
-                <Content style={{margin: '24px 16px 0'}}>
-                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
-                        content
-                        <Posts/>
-                    </div>
-                </Content>
-                <Footer style={{textAlign: 'center'}}>Manulife University Cohort 8 (2021)</Footer>
-            </Layout>
-        </Layout>
+        <div>
+            <Router>
+                <Layout style={{minHeight: '100vh'}}>
+                    <Sider>
+                        <h1 style={{
+                            marginLeft: 25,
+                            marginTop: 20,
+                            marginBottom: 15,
+                            color: 'rgba(255, 255, 255, 0.65)'
+                        }}>Anonymous Blog</h1>
+                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                            <Menu.Item key="1" icon={<HomeOutlined/>}>
+                                <Link to="/" onClick={() => setHeaderText('News Feed')}>Home</Link>
+                            </Menu.Item>
+                            <Menu.Item key="2" icon={<FormOutlined/>}>
+                                <Link to="/posts" onClick={() => setHeaderText('Posts')}>My Posts</Link>
+                            </Menu.Item>
+                            <Menu.Item key="3" icon={<CommentOutlined/>}>
+                                <Link to="/comments" onClick={() => setHeaderText('Comments')}>My Comments</Link>
+                            </Menu.Item>
+                            <Menu.Item key="4" icon={<LikeOutlined/>}>
+                                <Link to="/upvotes" onClick={() => setHeaderText('Upvotes')}>My Upvotes</Link>
+                            </Menu.Item>
+                            <Menu.Item key="5" icon={<DislikeOutlined/>}>
+                                <Link to="/downvotes" onClick={() => setHeaderText('Downvotes')}>My Downvotes</Link>
+                            </Menu.Item>
+                            <Menu.Item key="6" icon={<SettingOutlined/>}>
+                                <Link to="/preferences" onClick={() => setHeaderText('Preferences')}>Preferences</Link>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout>
+                        <Header className="site-layout-sub-header-background"
+                                style={{
+                                    height: 64,
+                                    textAlign: 'center',
+                                    color: "white",
+                                    fontSize: 20
+                                }}>{headerText}</Header>
+                        <Switch>
+                            <Route exact path="/">
+                                <Content style={{margin: '24px 16px 0', minHeight: '88vh'}}>
+                                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                                        <Posts/>
+                                    </div>
+                                </Content>
+                            </Route>
+
+                            <Route exact path="/posts">
+                                <Content style={{margin: '24px 16px 0', minHeight: '88vh'}}>
+                                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                                        SAMPLE POSTS
+                                    </div>
+                                </Content>
+                            </Route>
+
+                            <Route exact path="/comments">
+                                <Content style={{margin: '24px 16px 0', minHeight: '88vh'}}>
+                                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                                        SAMPLE COMMENTS
+                                    </div>
+                                </Content>
+                            </Route>
+
+                            <Route exact path="/upvotes">
+                                <Content style={{margin: '24px 16px 0', minHeight: '88vh'}}>
+                                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                                        SAMPLE UPVOTES
+                                    </div>
+                                </Content>
+                            </Route>
+
+                            <Route exact path="/downvotes">
+                                <Content style={{margin: '24px 16px 0', minHeight: '88vh'}}>
+                                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                                        SAMPLE DOWNVOTES
+                                    </div>
+                                </Content>
+                            </Route>
+
+                            <Route exact path="/preferences">
+                                <Content style={{margin: '24px 16px 0', minHeight: '88vh'}}>
+                                    <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
+                                        SAMPLE PREFERENCES
+                                    </div>
+                                </Content>
+                            </Route>
+                        </Switch>
+                        <Footer style={{textAlign: 'center', padding: 0}}>Manulife University Cohort 8 (2021)</Footer>
+                    </Layout>
+                </Layout>
+            </Router>
+        </div>
     )
 }
